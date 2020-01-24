6e6e9e2b53b50ccca7c957cfd4e05a510a3e291f
Screnhack <camelo_38@hotmail.com>
2020-01-24 09:43:08 -0500

    Agregado de getters

--- src/main/java/com/ceiba/adn/consultoriaabogados/aplicacion/comando/ConsultaAbogadoComando.java
+++ src/main/java/com/ceiba/adn/consultoriaabogados/aplicacion/comando/ConsultaAbogadoComando.java
@@ -1,5 +1,14 @@
 package com.ceiba.adn.consultoriaabogados.aplicacion.comando;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class ConsultaAbogadoComando {
 	private String nombre;
 	private String identificacion;
@@ -8,61 +17,5 @@ public class ConsultaAbogadoComando {
 	private String estado;
 	private String fechaConsulta;
 	private float precio;
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getIdentificacion() {
-		return identificacion;
-	}
-
-	public void setIdentificacion(String identificacion) {
-		this.identificacion = identificacion;
-	}
-
-	public String getCelular() {
-		return celular;
-	}
-
-	public void setCelular(String celular) {
-		this.celular = celular;
-	}
-
-	public String getTipoConsultoria() {
-		return tipoConsultoria;
-	}
-
-	public void setTipoConsultoria(String tipoConsultoria) {
-		this.tipoConsultoria = tipoConsultoria;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-
-	public String getFechaConsulta() {
-		return fechaConsulta;
-	}
-
-	public void setFechaConsulta(String fechaConsulta) {
-		this.fechaConsulta = fechaConsulta;
-	}
-
-	public float getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(float precio) {
-		this.precio = precio;
-	}
 
 }
