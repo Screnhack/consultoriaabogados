fd87c6dabf6df90f6523bddf71f0dc80f90c4aeb
Screnhack <camelo_38@hotmail.com>
2020-01-24 10:04:05 -0500

    Agregado de getters

--- src/main/java/com/ceiba/adn/consultoriaabogados/aplicacion/consulta/ListarConsultaAbogado.java
+++ src/main/java/com/ceiba/adn/consultoriaabogados/aplicacion/consulta/ListarConsultaAbogado.java
@@ -16,15 +16,4 @@ public class ListarConsultaAbogado {
 	private String estado;
 	private Date fechaConsulta;
 	
-	public ListarConsultaAbogado(Long id, String nombre, String identificacion, String celular, String tipoConsultoria,
-			String estado, Date fechaConsulta) {
-		this.id = id;
-		this.nombre = nombre;
-		this.identificacion = identificacion;
-		this.celular = celular;
-		this.tipoConsultoria = tipoConsultoria;
-		this.estado = estado;
-		this.fechaConsulta = fechaConsulta;
-	}
-
 }
