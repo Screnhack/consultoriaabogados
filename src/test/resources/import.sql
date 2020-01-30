44dd1a5068d1bab910e02d94d9640dd1bfe8ad28
Screnhack <camelo_38@hotmail.com>
2020-01-30 07:16:29 -0500

    Agregar correccion de error

---
+++ src/test/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO consulta (id, nombre, identificacion, celular, tipo_consultoria, estado, fecha_consulta, precio) VALUES (1, 'Andres Moreno', '1020456126', '31770501', 'FAMILIAR', 'PAGADA', '2020-01-28', 100000.00)
